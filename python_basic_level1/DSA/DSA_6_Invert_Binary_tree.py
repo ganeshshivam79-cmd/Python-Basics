4aacd3a90bda4cf448e797f69637f56c8fc54cac
ganeshshivam79-cmd <ganeshshivam79@gmail.com>
2026-01-29 21:31:05 +0530

    d

---
+++ python_basic_level1/DSA/DSA_6_Invert_Binary_tree.py
@@ -0,0 +1,19 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def invertTree(self, root):
+        if root is None:
+            return None
+        
+        # Swap the left and right subtrees
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
