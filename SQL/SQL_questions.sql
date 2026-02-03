f7eaf42be363a7538bc4d92b2b2f9d0eaa3f093e
ganeshshivam79-cmd <ganeshshivam79@gmail.com>
2026-02-03 20:11:02 +0530

    Update SQL_questions.sql

--- SQL/SQL_questions.sql
+++ SQL/SQL_questions.sql
@@ -17,3 +17,57 @@ WHERE MOD(rank_num, 2) = 0;  -- Even ranks only
 select * from ( select students_id, row_number() over (order by total_marks  desc) as ranks  from school) t where 
 mod(t.ranks,2)=0;
 
+
+SELECT customer_id
+FROM customers
+WHERE YEAR(order_date) = 2023 GROUP BY customer_id HAVING COUNT(DISTINCT MONTH(order_date)) = 12;
+
+
+select customer_id, sum(order_amount)/(select sum(order_amount) from customers) from customers
+group by customer_id 
+
+SELECT
+  product_id,
+  SUM(order_amount) AS product_revenue,
+  100.0 * SUM(order_amount) / NULLIF(SUM(SUM(order_amount)) OVER (), 0) AS revenue_pct
+FROM customers
+GROUP BY product_id
+ORDER BY revenue_pct DESC;
+
+
+WITH ranked_products AS (
+    SELECT
+        product_id,
+        SUM(revenue) OVER (
+            ORDER BY revenue DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) * 1.0 / SUM(revenue) OVER () AS cumulative_pct
+    FROM product_revenue
+)
+SELECT *
+FROM ranked_products
+WHERE cumulative_pct <= 0.8;
+
+
+
+WITH yearly AS (
+  SELECT
+    YEAR(order_date) AS order_date_year,
+    SUM(revenue) AS year_revenue
+  FROM customers
+  GROUP BY YEAR(order_date)
+)
+SELECT
+  order_date_year,
+  year_revenue,
+  year_revenue - LAG(year_revenue) OVER (ORDER BY order_date_year) AS year_growth
+FROM yearly
+ORDER BY [year];
+
+
+
+with below_revenue as ( select customer, ntile(10) over ( partition by customer_id order by total_amount desc ) as revenue from customer)
+select customer_id, revenue from below_revenue where revenue=10;
+
+select id, order_date, lag(order_date) over ( partition by id order by order_date) as prevdate from orders 
+where datetdiff(day, prevdate, order_date)=1;
