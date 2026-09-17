2a1e3269110a7157f2efb6746144b94b8c3ca1e1
ganeshshivam79-cmd <ganeshshivam79@gmail.com>
2026-09-17 13:06:00 +0530

    commit

--- python_basic_level1/DSA/merge_sort.py
+++ python_basic_level1/DSA/merge_sort.py
@@ -32,4 +32,25 @@ def merge_sort(val):
 
 print(merge_sort([10, 2, 3, 4, 9]))
 
+############################################################################
 
+def merge_two_lists(list1, list2):
+    result = []
+    i, j = 0, 0
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            result.append(list1[i])
+            i += 1
+        else:
+            result.append(list2[j])
+            j += 1
+    
+    # Add all remaining
+    result.extend(list1[i:])
+    result.extend(list2[j:])
+    
+    return result
+
+
+#################
